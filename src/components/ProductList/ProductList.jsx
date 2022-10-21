624f715566e4b2ff30ee808979c8f5c7b95fb24a
Colisey <colisey@inbox.ru>
2022-10-22 00:10:51 +0300

    q

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -17,42 +17,59 @@ const products = [
 
 const getTotalPrice = (items = []) => {
   return items.reduce((acc, item) => {
-    acc += item.price;
+    return acc += item.rpice;
   }, 0);
 };
 
 export const ProductList = () => {
   const tg = UseTelegram();
-  
+
   const [addedItems, setAddedItems] = useState([]);
+  // console.log('addedItems',addedItems)
+
   const onAdd = (product) => {
     // alert(product.price)
 
     const alreadyAdded = addedItems.find((i) => i.id === product.id);
-
     let newItems = [];
     // console.log('addedItems',addedItems);
     // console.log('alreadyAdded',alreadyAdded);
+    // console.log(alreadyAdded)
 
     if (alreadyAdded) {
+      
       newItems = addedItems.filter((i) => i.id !== product.id);
+      // console.log('if newItems',newItems)
     } else {
+      // alert('alreadyAdded else')
       newItems = [...addedItems, product];
+      // console.log('else newItems',newItems)
     }
     setAddedItems(newItems);
 
-    console.log('newItems.',newItems.length)
+    // console.log('newItems.',newItems.length)
 
+    // console.log('newItems.length',newItems.length)
+    // tg.MainButton.show();
 
-    if (newItems.length === 0) {
-      tg.MainButton.hide();
-    } else {
-      console.log('newItems true')
-      tg.MainButton.show();
-      tg.MainButton.setParams({
-        text: `Купить за ${getTotalPrice(newItems)}`,
-      });
-    }
+    useEffect(() => {
+      if (addedItems.length === 0) {
+        // console.log('newItems.',newItems.length)
+        tg.MainButton.hide();
+      } else {
+        tg.MainButton.show();
+      }
+    }, [addedItems]);
+
+    // if (newItems.length === 0) {
+    //   tg.MainButton.hide();
+    // } else {
+    //   // console.log('newItems true')
+    //   tg.MainButton.show();
+    //   // tg.MainButton.setParams({
+    //   //   text: `Купить за ${getTotalPrice(newItems)}`,
+    //   // });
+    // }
   };
 
   return (
