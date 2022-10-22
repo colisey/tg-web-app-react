933d0686ec29156b32622eeb89b3acaf3ac972ad
Colisey <colisey@inbox.ru>
2022-10-22 08:41:19 +0300

    Axios

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -4,15 +4,17 @@ import { ProductItem } from "../ProductItem/ProductItem";
 // import { ProductItem } from "../ProductItem/ProductItem";
 import styles from "./ProductList.module.css";
 
+import axios from "axios";
+
 const products = [
   { id: 1, title: "Куртка", price: 750, description: "Рыжая красивая" },
-  { id: 2, title: "Куртка 1", price: 1000, description: "Рыжая красивая" },
-  { id: 3, title: "Куртка 2", price: 1805, description: "Рыжая красивая" },
-  { id: 4, title: "Куртка 3", price: 3245, description: "Рыжая красивая" },
-  { id: 5, title: "Куртка 4", price: 879, description: "Рыжая красивая" },
-  { id: 6, title: "Куртка 5", price: 900, description: "Рыжая красивая" },
-  { id: 7, title: "Куртка 6", price: 1200, description: "Рыжая красивая" },
-  { id: 8, title: "Куртка 7", price: 1600, description: "Рыжая красивая" },
+  // { id: 2, title: "Куртка 1", price: 1000, description: "Рыжая красивая" },
+  // { id: 3, title: "Куртка 2", price: 1805, description: "Рыжая красивая" },
+  // { id: 4, title: "Куртка 3", price: 3245, description: "Рыжая красивая" },
+  // { id: 5, title: "Куртка 4", price: 879, description: "Рыжая красивая" },
+  // { id: 6, title: "Куртка 5", price: 900, description: "Рыжая красивая" },
+  // { id: 7, title: "Куртка 6", price: 1200, description: "Рыжая красивая" },
+  // { id: 8, title: "Куртка 7", price: 1600, description: "Рыжая красивая" },
 ];
 
 const getTotalPrice = (items = []) => {
@@ -25,6 +27,51 @@ export const ProductList = () => {
   const { tg } = UseTelegram();
 
   const [addedItems, setAddedItems] = useState([]);
+  const [appState, setAppState] = useState();
+
+  useEffect(() => {
+
+    axios
+      .post("https://suppliers-api.wildberries.ru/content/v1/cards/filter", {
+        "id": 1,
+        "jsonrpc": "2.0",
+        "params": {
+          "filter": {
+            "filter": [
+              {
+                "column": "string",
+                "excludedValues": {}
+              }
+            ],
+            "find": [
+              {
+                "column": "string",
+                "search": {}
+              }
+            ],
+            "order": {
+              "column": "string",
+              "order": "string"
+            }
+          },
+          "isArchive": true,
+          "query": {
+            "limit": 0,
+            "offset": 0,
+            "total": 0
+          },
+          "supplierID": "NWY3ZGE5Y2MtYWM1YS00NjVmLWIxMTItODNmZTZjYWQ1ZWU5",
+          "withError": true
+        }
+      })
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }, []);
+
   // console.log('addedItems',addedItems)
   // tg.MainButton.show();
 
@@ -79,6 +126,7 @@ export const ProductList = () => {
 
   return (
     <div className={styles.list}>
+      <button onClick={setAppState}>Загрузить</button>
       {products.map((item) => (
         <ProductItem product={item} onAdd={onAdd} className={"item"} />
       ))}
