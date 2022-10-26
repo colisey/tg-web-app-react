1e1636bc34de45f798e30391642faa355a0b0eee
Colisey <colisey@inbox.ru>
2022-10-26 20:04:56 +0300

    ddadad

--- src/components/Gallery/Gallery.tsx
+++
@@ -1,9 +0,0 @@
-import { FC } from "react";
-import { GalleryProps } from ".";
-import styles from "./Gallery.module.css";
-
-export const Gallery: FC<GalleryProps> = (props) => {
-  return (
-    <div className={styles.wrapper} {...props}>Gallery</div>
-  );
-};
--- src/core/types/types.ts
+++
@@ -1,5 +0,0 @@
-
-import React from "react";
-export interface ButtonProps extends React.DetailedHTMLProps<React.ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement>, React.AriaAttributes  {}
-export interface InputEventType extends React.ChangeEvent<HTMLInputElement> {}
-export interface SelectElementType extends React.ChangeEvent<HTMLSelectElement> {}
