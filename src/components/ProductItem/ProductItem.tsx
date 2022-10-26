c95eed548a3fcaf71f9be81a363a69df369c1b67
Colisey <colisey@inbox.ru>
2022-10-26 16:57:16 +0300

    ts + links

--- src/components/ProductItem/ProductItem.tsx
+++ src/components/ProductItem/ProductItem.tsx
@@ -1,12 +1,18 @@
+import { FC } from "react";
+import { ProductItemProps } from "./ProductItem.props";
 import { Button } from "../Button/Button";
 import styles from "./ProductItem.module.css";
 
-export const ProductItem = ({product, className, onAdd}) => {
+export const ProductItem: FC<ProductItemProps> = ({
+  product,
+  className,
+  onAdd,
+}) => {
   // console.log('ProductItem');
 
   const onAddHandler = () => {
-    onAdd(product)
-  }
+    onAdd(product);
+  };
   // console.log('ProductItem');
   return (
     <div className={styles.wrapper}>
@@ -14,7 +20,9 @@ export const ProductItem = ({product, className, onAdd}) => {
       <div className={styles.title}>{product.title}</div>
       <div className={styles.description}>{product.description}</div>
       <div className={styles.price}>
-        <span>Стоимость <b>{product.price}</b>руб.</span>
+        <span>
+          Стоимость <b>{product.price}</b>руб.
+        </span>
       </div>
 
       <Button className={styles.addBtn} onClick={onAddHandler}>
--- src/core/types/types.ts
+++ src/core/types/types.ts
@@ -1,3 +1,5 @@
 
 import React from "react";
 export interface ButtonProps extends React.DetailedHTMLProps<React.ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement>, React.AriaAttributes  {}
+export interface InputEventType extends React.ChangeEvent<HTMLInputElement> {}
+export interface SelectElementType extends React.ChangeEvent<HTMLSelectElement> {}
