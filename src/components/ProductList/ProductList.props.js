7bc8b96b146a3e61a8118f6e2b59494a7304a220
Colisey <colisey@inbox.ru>
2022-10-26 19:15:59 +0300

    ghj,f

---
+++ src/components/ProductList/ProductList.props.js
@@ -0,0 +1,10 @@
+// export interface ProductListProps {
+    
+// };
+// export interface itemType {
+//     id: number;
+//     title: string;
+//     price: number;
+//     description: string;
+//   }
+//   export type ProductsType = itemType[];
