dc1d79eeb8ded3fdf29c45a609bbea141503961f
Colisey <colisey@inbox.ru>
2022-10-26 19:32:31 +0300

    dada

---
+++ src/api/api.js
@@ -0,0 +1,26 @@
+import axios from "axios";
+import { TOKEN } from "./wb";
+// import { TOKEN } from "";
+// import React, { useEffect, useState } from "react";
+
+const headers = {
+    "Content-Type": "application/json",
+    Authorization: TOKEN,
+  };
+export const API = axios.create({
+    baseURL: 'https://suppliers-api.wildberries.ru//cursor/list',
+    timeout: 1000,
+    headers
+  });
+
+
+export const products = [
+    { id: 1, title: "Куртка", price: 750, description: "Рыжая красивая" },
+    { id: 2, title: "Куртка 1", price: 1000, description: "Рыжая красивая" },
+    { id: 3, title: "Куртка 2", price: 1805, description: "Рыжая красивая" },
+    { id: 4, title: "Куртка 3", price: 3245, description: "Рыжая красивая" },
+    { id: 5, title: "Куртка 4", price: 879, description: "Рыжая красивая" },
+    { id: 6, title: "Куртка 5", price: 900, description: "Рыжая красивая" },
+    { id: 7, title: "Куртка 6", price: 1200, description: "Рыжая красивая" },
+    { id: 8, title: "Куртка 7", price: 1600, description: "Рыжая красивая" },
+  ];
