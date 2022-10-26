410d1cf2e7cf66d238d8f117564d799ca5e250ce
Colisey <colisey@inbox.ru>
2022-10-26 20:38:43 +0300

    trtrt

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 // import { FC } from "react";
 // import { ButtonProps } from "../../core/types/types";
 import styles from "./Button.module.css";
@@ -5,3 +6,10 @@ import styles from "./Button.module.css";
 export const Button= (props) => { //: FC<ButtonProps> 
   return <button className={styles.wrapper} {...props} />;
 };
+=======
+import styles from "./Button.module.css";
+
+export const Button = (props) => {
+  return <button className={styles.wrapper} {...props} />;
+};
+>>>>>>> parent of c66035d... trtrt
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 // import { getTTFB } from "web-vitals";
 // import { FC } from "react";
 import { UseTelegram } from "../../core/hooks/useTelegram";
@@ -16,6 +17,25 @@ const {user, onClose} = UseTelegram();
         <div>{user?.username}</div>
         {/* <div>{user?.photo_url}</div> */}
         <span>Версия 04</span>
+=======
+import { getTTFB } from "web-vitals";
+import { UseTelegram } from "../../hooks/useTelegram";
+import { Button } from "../Button/Button";
+import styles from "./Header.module.css";
+
+export const Header = (props) => {
+const {user, onClose} = UseTelegram();
+
+ 
+
+  return (
+    <div className={styles.wrapper} {...props}>
+      <Button onClick={onClose}>Закрыть окно</Button>
+      <div className={styles.username}>
+        <div>{user?.username}</div>
+        <div>{user?.photo_url}</div>
+        <span>Версия 03</span>
+>>>>>>> parent of c66035d... trtrt
         {/* <div>{JSON.stringify(tg.initDataUnsafe?.user)}</div> */}
       
         
