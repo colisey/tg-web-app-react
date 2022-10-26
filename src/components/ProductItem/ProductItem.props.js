046f818978c3d2a4ebf3faa941cd4727e5ddfbb3
Colisey <colisey@inbox.ru>
2022-10-26 20:39:11 +0300

    trtrt

--- src/components/ProductItem/ProductItem.props.js
+++
@@ -1,7 +0,0 @@
-// import { itemType } from "../ProductList";
-
-// export interface ProductItemProps {
-//     product: itemType,
-//     className: string,
-//     onAdd: (e : itemType) => void,
-// };
