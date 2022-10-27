aad671766be232507b4ca2d05fc0ca6211aac480
Colisey <colisey@inbox.ru>
2022-10-27 20:57:49 +0300

    Галлерея 01

--- src/components/Cases/Case/Case.jsx
+++ src/components/Cases/Case/Case.jsx
@@ -7,20 +7,32 @@ export const Case = ({ first_name, last_name, avatar, email }) => {
   return (
     <div className={styles.wrapper}>
       <div>
-        <img src={avatar} alt={name} />
+        <img className={styles.img} src={avatar} alt={name} />
       </div>
       <div>
         <strong>{name}</strong>
-        <p className={styles.p}>Некий текст который тут будет</p>
+        <p className={styles.p}>
+          Lorem ipsum dolor sit amet, <strong>consectetur adipiscing elit</strong>, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Sed
+          faucibus turpis in eu. <strong>Nunc sed blandit libero</strong> volutpat sed cras
+          ornare arcu. Lectus magna fringilla urna porttitor rhoncus dolor.
+          Rhoncus est pellentesque elit ullamcorper dignissim cras tincidunt.
+        </p>
         <ul className={styles.ul}>
           <li className={styles.li}>
-            <a className={styles.a} href="tel:+79778374262">Телефон: +7(977)837-42-62</a>
+            <a className={styles.a} href="tel:+79778374262">
+              Телефон: <span>+7(977)837-42-62</span>
+            </a>
           </li>
           <li className={styles.li}>
-            <a className={styles.a} href="@FilichevValeriy">Телеграм: @FilichevValeriy</a>
+            <span className={styles.a}>
+              Телеграм: <span>@FilichevValeriy</span>
+            </span>
           </li>
           <li className={styles.li}>
-            <a className={styles.a} href={`mailto:${styles.mail}`}>Написать на Mail</a>
+            <a className={styles.a} href={`mailto:${styles.mail}`}>
+              Написать на Mail
+            </a>
           </li>
         </ul>
       </div>
